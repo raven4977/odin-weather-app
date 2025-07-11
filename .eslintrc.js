b792f016d478b1cefa656194b4e0ebe0a2b1181b
Cameron Brown <moontsukuyomi6@gmail.com>
2025-07-11 14:20:23 +0200

    eslint --fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,10 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base'],
   parserOptions: {
     ecmaVersion: 15,
     sourceType: 'module',
   },
-  rules: {
-  },
+  rules: {},
 };
