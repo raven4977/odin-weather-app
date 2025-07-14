b1d301fc4269b2074e73f02dd253c16e079af73b
Cameron Brown <moontsukuyomi6@gmail.com>
2025-07-14 10:34:02 +0200

    Update weather.js fetch to include unit group

--- src/weather-data.js
+++ src/weather-data.js
@@ -1,7 +1,7 @@
-export default async function getWeather(location) {
+export default async function getWeather(location, unit) {
   try {
     const response = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=FVEHGEC5HH58QLVUQYFAZN2VJ`,
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=${unit}&key=FVEHGEC5HH58QLVUQYFAZN2VJ`
     );
 
     if (!response.ok) {
