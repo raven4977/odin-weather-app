ad553396281321e16d52921070d00fc7682f5e17
Cameron Brown <moontsukuyomi6@gmail.com>
2025-07-14 10:58:47 +0200

    Call functions in index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-import "./styles.css";
+import './styles.css';
+import getWeather from './weather-data';
+import events from './events';
+
+getWeather('London, UK');
+
+const handleEvents = events();
+
+handleEvents.search();
+
+handleEvents.toggleTemp();
